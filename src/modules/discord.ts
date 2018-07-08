25cf3a78c70433e0ca7d5b89f6d1a479bd5941c2
Gabe <kuslanskyg@gmail.com>
2018-07-08 00:47:06 -0400

    Made postToDiscord asynchronous

--- src/modules/discord.ts
+++ src/modules/discord.ts
@@ -6,15 +6,12 @@ import * as request from 'request';
  * @param {string} message - The message to send.
  * @param {string} channel - The channel to send the message in.
  * @param {boolean} [fancy=false] - If fancy = true, put the message contents in a Discord embed
- * ('false' if unspecified).
- * @param {() => void} [done] - The callback function that runs if the message successfully sends.
  */
 // tslint:disable-next-line:max-line-length
-export function postToDiscord(message: string, channel: string, fancy = false, done?: () => void): void {
-  const webhook = config.get(`discord.webhooks.${channel}`);
-  if (webhook !== undefined) {
+export async function postToDiscord(message: string, channel: string, fancy = false): Promise<void> {
+  if (config.get(`discord.webhooks.${channel}`) !== undefined) {
     const data = {
-      url: webhook,
+      url: config.get(`discord.webhooks.${channel}`),
       method: 'POST',
       body: {
         avatar_url: '',
@@ -24,12 +21,10 @@ export function postToDiscord(message: string, channel: string, fancy = false, d
       },
       json: true,
     };
-    request(data, (err) => {
+    await request(data, (err) => {
       if (err) {
         console.warn(`[Webhook] ${err}`);
       }
-      // Run callback if exists
-	    (done || Function)();
     });
   } else {
   	// Log something error related
