f958349f518486574047a76e3c8c720ca4eaa55c
Gabe <kuslanskyg@gmail.com>
2018-07-09 20:18:54 -0400

    Created global express module Converted require statements to imports

--- src/modules/routing.ts
+++ src/modules/routing.ts
@@ -3,11 +3,28 @@ const steam = require('steam-login');
 
 const router: Router = Router();
 
+declare module 'express' {
+  export interface Request {
+    user: any;
+  }
+}
+
 router.get('/', (req: Request, res: Response) => {
   res.send('Hi!');
 });
 
 router.get('/verify', steam.verify(), (req: Request, res: Response) => {
+  // Arrange data from login
+  const steamid = req.user.steamid.toString();
+  const avatar = req.user.avatar.medium;
+  const ip = req.headers['x-forwarded-for'];
+
+  // Insert player into database
+  // Add IP to the user's IP list if current ip doesn't exist for this SteamID
+
+  // Check if player is banned
+
+  // Send player to website if they are not banned
   res.redirect('/');
 });
 
