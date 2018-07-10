9e386efb4d5d49c2b921adffa1ae771ca2c377da
Nick <nick.win999@gmail.com>
2018-07-09 21:30:39 -0500

    Remove unnecessary duplicate code of the NavigationItem component

--- src/components/Navigation/NavigationItem/index.tsx
+++ src/components/Navigation/NavigationItem/index.tsx
@@ -16,44 +16,24 @@ class NavigationItem extends React.Component<NavigationItemProps, {}> {
                 <div className="navDivider" />
             );
         } else if (this.props.properties.type === 'tab' && this.props.properties.tabConfig) {
-            if (this.props.properties.tabConfig.external) {
-                return (
-                    <a 
-                        className="navTab"
-                        href={this.props.properties.tabConfig.link} 
-                        data-tooltip={this.props.properties.tabConfig.name} 
-                        data-tooltip-position="right"
-                        target="blank"
-                    >
-                        <div>
-                            <FontAwesomeIcon
-                                icon={
-                                    [this.props.properties.tabConfig.iconPrefix, this.props.properties.tabConfig.icon]
-                                }
-                            />
-                        </div>
-                        <span>{this.props.properties.tabConfig.name}</span>
-                    </a>
-                );
-            } else if (!this.props.properties.tabConfig.external) {
-                return (
-                    <Link
-                        className="navTab" 
-                        to={this.props.properties.tabConfig.link} 
-                        data-tooltip={this.props.properties.tabConfig.name}
-                        data-tooltip-position="right"
-                    >
-                        <div>
-                            <FontAwesomeIcon 
-                                icon={
-                                    [this.props.properties.tabConfig.iconPrefix, this.props.properties.tabConfig.icon]
-                                }
-                            />
-                        </div>
-                        <span>{this.props.properties.tabConfig.name}</span>
-                    </Link>
-                );
-            }
+            return (
+                <Link
+                    className="navTab" 
+                    to={this.props.properties.tabConfig.link} 
+                    data-tooltip={this.props.properties.tabConfig.name}
+                    data-tooltip-position="right"
+                    target={this.props.properties.tabConfig.external ? '_blank' : ''}
+                >
+                    <div>
+                        <FontAwesomeIcon 
+                            icon={
+                                [this.props.properties.tabConfig.iconPrefix, this.props.properties.tabConfig.icon]
+                            }
+                        />
+                    </div>
+                    <span>{this.props.properties.tabConfig.name}</span>
+                </Link>
+            );
         } else if (this.props.properties.type === 'module') {
             return (null);
         }
