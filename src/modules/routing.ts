457d63cb6067093fe32f7896bdf276cc5d3cf637
Nick <nick.win999@gmail.com>
2018-07-14 13:11:17 -0500

    Complete auth circuit with front end

--- src/modules/routing.ts
+++ src/modules/routing.ts
@@ -1,9 +1,12 @@
 import { Router, Request, Response } from 'express';
+import * as config from 'config';
 import { steamUser } from '../common/types';
 import { loginUser } from './login';
 import * as steam from 'steam-login';
 
 const router: Router = Router();
+
+const frontURL: string = config.get('app.frontURL');
 
 router.get('/', (req: Request, res: Response) => {
   res.send('Hi!');
@@ -11,18 +14,18 @@ router.get('/', (req: Request, res: Response) => {
 
 router.get('/verify', steam.verify(), (req: Request & steamUser, res: Response) => {
   loginUser(req).then((banned) => {
-    banned ? res.redirect('logout?banned=true') : res.redirect('/');
+    banned ? res.redirect('logout?banned=true') : res.redirect(frontURL);
   }).catch(e => console.log(e));
 });
 
 router.get('/auth', steam.authenticate(), (req: Request & steamUser, res: Response) => {
-  res.redirect('/');
+  res.redirect(frontURL);
 });
 
 router.get('/logout', steam.enforceLogin('/'), (req: Request & steamUser, res: Response) => {
   delete req.session.steamUser;
   req.user = null;
-  res.redirect('/');
+  res.redirect(frontURL);
 });
 
 export const routing: Router = router;
