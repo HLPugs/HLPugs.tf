2c6dfaa601636374d998de5299a380898bad6e5e
Nick <nick.win999@gmail.com>
2018-07-14 20:17:52 -0500

    Add simple chat history

--- src/modules/sockets/all/chat.ts
+++ src/modules/sockets/all/chat.ts
@@ -1,11 +1,21 @@
 import { Server } from 'socket.io';
 import * as uuid from 'uuid';
 
+interface messageObjectType {
+  message: string;
+  username: string;
+  userid: string;
+  id: string;
+  timestamp: number;
+}
+
+let messageHistory: messageObjectType[] = [];
+
 export const chat = (io: Server) => {
   io.on('connection', (socket) => {
     socket.on('sendMessage', (message: string) => {
       if (message.length && socket.request.session.steamUser) {
-        const messageObject = {
+        const messageObject: messageObjectType = {
           message,
           username: socket.request.session.steamUser.username, // Placeholder waiting for names
           userid: socket.request.session.steamUser.steamid,
@@ -13,8 +23,18 @@ export const chat = (io: Server) => {
           timestamp: new Date().getTime(),
         };
 
+        messageHistory.push(messageObject);
+
+        if (messageHistory.length > 100) {
+          messageHistory = messageHistory.slice(1);
+        }
+
         io.emit('newMessage', messageObject);
       }
+    });
+
+    socket.on('requestMessageHistory', () => {
+      socket.emit('messageHistory', messageHistory);
     });
   });
 };
