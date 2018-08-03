a779cae3f75d93bb560a8b0656d200f009171ba1
Nick <nick.win999@gmail.com>
2018-08-02 22:53:52 -0500

    Somewhat improved AutoCompletions

--- src/components/Chat/ChatInput/AutoCompletions/index.tsx
+++ src/components/Chat/ChatInput/AutoCompletions/index.tsx
@@ -11,79 +11,86 @@ interface AutoCompletionsProps {
 }
 
 class AutoCompletion extends React.Component<AutoCompletionsProps, {}> {
+    completionsCollection = () => {
+        if (this.props.emojiCompletions.length) {
+            return (
+                <>
+                    {this.props.emojiCompletions.map((completion: CompletionItem, index: number) =>
+                        this.createCompletion(index, completion)
+                    )}
+                </>
+            );
+        } else if (this.props.mentionCompletions.length) {
+            return (
+                <>
+                    {this.props.mentionCompletions.map((completion: string, index: number) =>
+                        this.createCompletion(index, completion)
+                    )}
+                </>
+            );
+        }
+        return null;
+    }
+
+    createCompletion = (index: number, completion: string | CompletionItem) => {
+        return (
+            <div
+                className={this.props.autoCompleteIndex === index ? 'autoCompletionItemHighlight' : ''}
+                key={index}
+                onClick={() => this.handleClick(index)}
+            >
+                {this.createCompletionContent(completion)}
+            </div>
+        );
+    }
+
+    createCompletionContent = (completion: string | CompletionItem) => {
+        if (typeof completion === 'string') {
+            return <span>{completion}</span>;
+        } else if (completion.name) {
+            return (
+                <>
+                    <Emoji
+                        emoji={`:${completion.name}:`}
+                        size={20}
+                        set="twitter"
+                        sheetSize={32}
+                        tooltip={true}
+                    />
+                    <span>{`:${completion.name}:`}</span>
+                </>
+            );
+        } else if (completion.customName) {
+            return (
+                <>
+                    <span
+                        title={completion.customName}
+                        className="emoji-mart-emoji"
+                    >
+                        <span
+                            className="customEmoji"
+                            style={{ backgroundImage: `url(${completion.url})` }}
+                        />
+                    </span>
+                    <span>{`:${completion.customName}:`}</span>
+                </>
+            );
+        }
+        return null;
+    }
+
     handleClick = (index: number) => {
         this.props.completionClick(index);
     }
 
     render() {
-        if (this.props.emojiCompletions.length) {
+        if (this.props.emojiCompletions.length || this.props.mentionCompletions.length) {
             return (
                 <div id="autoCompletionHolder">
-                    {this.props.emojiCompletions.map((completion: CompletionItem, index: number) => {
-                            if (completion.name) {
-                                return (
-                                    <div 
-                                        className={
-                                            this.props.autoCompleteIndex === index ? 'autoCompletionItemHighlight' : ''
-                                        } 
-                                        key={index}
-                                        onClick={() => this.handleClick(index)}
-                                    >
-                                        <Emoji 
-                                            emoji={`:${completion.name}:`} 
-                                            size={20} 
-                                            set="twitter" 
-                                            sheetSize={32} 
-                                            tooltip={true} 
-                                        />
-                                        <span>{`:${completion.name}:`}</span>
-                                    </div>
-                                );
-                            } else if (completion.customName) {
-                                return (
-                                    <div
-                                        className={
-                                            this.props.autoCompleteIndex === index ? 'autoCompletionItemHighlight' : ''
-                                        }
-                                        key={index}
-                                        onClick={() => this.handleClick(index)}
-                                    >
-                                        <span
-                                            title={completion.customName}
-                                            className="emoji-mart-emoji"
-                                            key={index}
-                                        >
-                                            <span 
-                                                className="customEmoji" 
-                                                style={{ backgroundImage: `url(${completion.url})` }} 
-                                            />
-                                        </span>
-                                        <span>{`:${completion.customName}:`}</span>
-                                    </div>
-                                );
-                            } else {
-                                return null;
-                            }
-                        }
-                    )}
-                </div>
-            );
-        } else if (this.props.mentionCompletions.length) {
-            return (
-                <div id="autoCompletionHolder">
-                    {this.props.mentionCompletions.map((completion: string, index: number) =>
-                        <div 
-                            className={this.props.autoCompleteIndex === index ? 'autoCompletionItemHighlight' : ''} 
-                            key={index}
-                            onClick={() => this.handleClick(index)}
-                        >
-                            <span>{`${completion}`}</span>
-                        </div>
-                    )}
+                    {this.completionsCollection()}
                 </div>
             );
         }
-
         return <div />;
     }
 }
