76c5e6b7fa3968225c9c60cbbc81de2b0ff87e64
Nick <nick.win999@gmail.com>
2018-08-02 21:31:22 -0500

    Complete SearchEmojis function refactor

--- src/components/Chat/ChatInput/SearchEmojis.ts
+++ src/components/Chat/ChatInput/SearchEmojis.ts
@@ -15,68 +15,65 @@ const notInCompletions = (query: string, completions: CompletionItem[]) => {
 const SearchEmojis = (fragment: string, input: React.RefObject<HTMLTextAreaElement>, customEmojis: CustomEmoji[]) => {
     const query = fragment.slice(1);
 
-    if (fragment.startsWith(':') && query.length > 1) {
-        let completions: CompletionItem[] = [];
-
-        for (const emoji of customEmojis) {
-            if (completions.length >= 8) {
-                break;
-            }
-
-            if (emoji.short_names[0].substring(0, query.length) === query) {
-                completions.push({
-                    customName: emoji.short_names[0],
-                    url: emoji.imageUrl
-                });
-            }
-        }
-
-        for (const emoji in allEmojis.emojis) {
-            if (emoji.substring(0, query.length) === query) {
-                if (completions.length >= 8) {
-                    break;
-                }
-
-                completions.push({
-                    name: emoji
-                });
-            }
-        }
-
-        for (const emoji of customEmojis) {
-            if (completions.length >= 8) {
-                break;
-            }
-
-            if (emoji.short_names[0].includes(query) && notInCompletions(query, completions)) {
-                completions.push({
-                    customName: emoji.short_names[0],
-                    url: emoji.imageUrl
-                });
-            }
-        }
-
-        for (const emoji in allEmojis.emojis) {
-            if (emoji.includes(query) && notInCompletions(query, completions)) {
-                if (completions.length >= 8) {
-                    break;
-                }
-
-                completions.push({
-                    name: emoji
-                });
-            }
-        }
-
+    if (!fragment.startsWith(':') || query.length <= 1) {
+        // If query is only 1 character or isn't actually a emoji search, return with blank
         return {
             autoCompleteIndex: 0,
-            emojiCompletions: completions
+            emojiCompletions: []
         };
+    }
+
+    const completions: CompletionItem[] = [];
+
+    // Add CUSTOM emojis where BEGINNING MATCHES query
+    for (const emoji of customEmojis) {
+        if (completions.length >= 8) { break; }
+
+        if (emoji.short_names[0].substring(0, query.length) === query) {
+            completions.push({
+                customName: emoji.short_names[0],
+                url: emoji.imageUrl
+            });
+        }
+    }
+
+    // Add BASE emojis where BEGINNING MATCHES query
+    for (const emoji in allEmojis.emojis) {
+        if (emoji.substring(0, query.length) === query) {
+            if (completions.length >= 8) { break; }
+
+            completions.push({
+                name: emoji
+            });
+        }
+    }
+
+    // Add CUSTOM emojis where ANY SECTION MATCHES query
+    for (const emoji of customEmojis) {
+        if (completions.length >= 8) { break; }
+
+        if (emoji.short_names[0].includes(query) && notInCompletions(query, completions)) {
+            completions.push({
+                customName: emoji.short_names[0],
+                url: emoji.imageUrl
+            });
+        }
+    }
+
+    // Add BASE emojis where ANY SECTION MATCHES query
+    for (const emoji in allEmojis.emojis) {
+        if (emoji.includes(query) && notInCompletions(query, completions)) {
+            if (completions.length >= 8) { break; }
+
+            completions.push({
+                name: emoji
+            });
+        }
     }
 
     return {
         autoCompleteIndex: 0,
-        emojiCompletions: []
+        emojiCompletions: completions
     };
 };
 
