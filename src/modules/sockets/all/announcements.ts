a54492d86562b4dd4e59587cd21bc866879c18e1
Gabe Kuslansky <kuslanskyg@gmail.com>
2019-07-06 17:54:32 -0400

    Simplified announcements call

--- src/modules/sockets/all/announcements.ts
+++ src/modules/sockets/all/announcements.ts
@@ -5,9 +5,8 @@ import db                       from '../../../database/db';
 // @ts-ignore
 const defaultRegion: Region = process.env.region;
 
-export async function getAnnouncements(customRegion?: Region): Promise<Announcement[]> {
+export async function getAnnouncements(region: Region = defaultRegion): Promise<Announcement[]> {
   // Populate in-memory announcements collection
-  const region = customRegion !== undefined ? customRegion : defaultRegion;
   const query = {
     text: 'SELECT content, priority FROM announcements WHERE region = $1 ORDER BY timestamp DESC',
     values: [region],
@@ -17,11 +16,10 @@ export async function getAnnouncements(customRegion?: Region): Promise<Announcem
   return rows;
 }
 
-export const announcements = async (io: Server) => {
 
+export const announcements = async (io: Server) => {
   io.on('connection', (socket) => {
-    socket.on('loadAnnouncements', async(customRegion?: Region) => {
-      const region = customRegion !== undefined ? customRegion : defaultRegion;
+    socket.on('loadAnnouncements', async(region: Region = defaultRegion) => {
       const announcementList = await getAnnouncements(region);
       socket.emit('receiveAnnouncements', announcementList);
     });
