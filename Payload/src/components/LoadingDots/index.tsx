885a0e115e0559da9bbf98d9aa34649b006a56cf
Nick <nick.win999@gmail.com>
2019-08-18 17:58:20 -0500

    Add basic profile page

---
+++ Payload/src/components/LoadingDots/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './style.scss';
+
+class LoadingDots extends React.PureComponent {
+  render() {
+    return (
+      <div id="loadingDots">
+        <div className="loadingDotHolder">
+          <div className="loadingDot" />
+        </div>
+        <div className="loadingDotHolder" style={{ '--delay': '.1s' } as React.CSSProperties}>
+          <div className="loadingDot" />
+        </div>
+        <div className="loadingDotHolder" style={{ '--delay': '.2s' } as React.CSSProperties}>
+          <div className="loadingDot" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LoadingDots;
--- Payload/src/components/Profile/index.tsx
+++ Payload/src/components/Profile/index.tsx
@@ -1,31 +1,70 @@
 import React from 'react';
 
 import './style.scss';
-import { RouteComponentProps } from 'react-router-dom';
+import HttpClient from '../../common/HttpClient';
+import { ProfileViewModel } from '../../../../common/ViewModels/ProfileViewModel';
+import LoadingDots from '../LoadingDots';
 
 interface ProfileProps {
   steamid: string;
 }
 
-class Profile extends React.Component<ProfileProps, {}> {
+interface ProfileState {
+  loading: boolean;
+  player?: ProfileViewModel;
+}
+
+class Profile extends React.Component<ProfileProps, ProfileState> {
+  http: HttpClient;
+
+  constructor(props: ProfileProps) {
+    super(props);
+
+    this.state = {
+      loading: true
+    };
+
+    this.http = new HttpClient();
+  }
+
+  componentDidMount() {
+    this.getPlayerData();
+  }
+
+  getPlayerData = async () => {
+    this.setState({
+      loading: true
+    });
+
+    const player = await this.http.get(`/profile/${this.props.steamid}`);
+
+    this.setState({
+      loading: false,
+      player
+    });
+  }
+
   render() {
-    return (
+    return this.state.player ? (
       <main>
         <div className="profileWindow">
           <div className="profile">
             <div className="identity">
               <a
                 href={`https://steamcommunity.com/profiles/${this.props.steamid}`}
+                style={{backgroundImage: `url('${this.state.player.avatarUrl}')`}}
                 className="profilePicture"
                 target="_blank"
+                rel="noopener noreferrer"
               />
               <div className="profileInfo">
-                <div className="alias">Nicell</div>
+                <div className="alias">{this.state.player.alias}</div>
                 <div className="quickLinks">
                   <a
                     className="quickLink"
                     href={`https://steamcommunity.com/profiles/${this.props.steamid}`}
                     target="_blank"
+                    rel="noopener noreferrer"
                   >
                     <span>Steam</span>
                   </a>
@@ -33,6 +72,7 @@ class Profile extends React.Component<ProfileProps, {}> {
                     className="quickLink"
                     href={`https://logs.tf/profile/${this.props.steamid}`}
                     target="_blank"
+                    rel="noopener noreferrer"
                   >
                     <span>Logs.tf</span>
                   </a>
@@ -40,6 +80,7 @@ class Profile extends React.Component<ProfileProps, {}> {
                     className="quickLink"
                     href={`https://demos.tf/profiles/${this.props.steamid}`}
                     target="_blank"
+                    rel="noopener noreferrer"
                   >
                     <span>Demos.tf</span>
                   </a>
@@ -47,6 +88,7 @@ class Profile extends React.Component<ProfileProps, {}> {
                     className="quickLink"
                     href={`http://hl.rgl.gg/Public/PlayerProfile.aspx?p=${this.props.steamid}`}
                     target="_blank"
+                    rel="noopener noreferrer"
                   >
                     <span>RGL.gg</span>
                   </a>
@@ -54,6 +96,7 @@ class Profile extends React.Component<ProfileProps, {}> {
                     className="quickLink"
                     href={`https://etf2l.org/search/${this.props.steamid}`}
                     target="_blank"
+                    rel="noopener noreferrer"
                   >
                     <span>ETF2L</span>
                   </a>
@@ -61,6 +104,7 @@ class Profile extends React.Component<ProfileProps, {}> {
                     className="quickLink"
                     href={`http://www.ugcleague.com/players_page.cfm?player_id=${this.props.steamid}`}
                     target="_blank"
+                    rel="noopener noreferrer"
                   >
                     <span>UGC</span>
                   </a>
@@ -68,6 +112,12 @@ class Profile extends React.Component<ProfileProps, {}> {
               </div>
             </div>
           </div>
+        </div>
+      </main>
+    ) : (
+      <main>
+        <div className="loadingHolder">
+          <LoadingDots/>
         </div>
       </main>
     );
