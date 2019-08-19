3a38e68402dfc2e5a3decc31944762ee432345fa
Nick <nick.win999@gmail.com>
2019-08-18 20:54:17 -0500

    Add chart for class pug stats on profile

--- Payload/src/components/Profile/index.tsx
+++ Payload/src/components/Profile/index.tsx
@@ -5,6 +5,59 @@ import HttpClient from '../../common/HttpClient';
 import { ProfileViewModel } from '../../../../common/ViewModels/ProfileViewModel';
 import LoadingDots from '../LoadingDots';
 
+import {BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, TooltipPayload} from 'recharts';
+import ClassIcon from '../ClassIcon';
+import DraftTFClass from '../../../../common/Models/DraftTFClass';
+
+const data = [
+  {
+    TFClass: 'Scout',
+    wins: 246,
+    ties: 20,
+    losses: 274
+  }, {
+    TFClass: 'Soldier',
+    wins: 85,
+    ties: 8,
+    losses: 93
+  }, {
+    TFClass: 'Pyro',
+    wins: 26,
+    ties: 2,
+    losses: 24
+  }, {
+    TFClass: 'Demo',
+    wins: 174,
+    ties: 14,
+    losses: 186
+  }, {
+    TFClass: 'Heavy',
+    wins: 31,
+    ties: 4,
+    losses: 32
+  }, {
+    TFClass: 'Engineer',
+    wins: 26,
+    ties: 2,
+    losses: 29
+  }, {
+    TFClass: 'Medic',
+    wins: 8,
+    ties: 1,
+    losses: 19
+  }, {
+    TFClass: 'Sniper',
+    wins: 33,
+    ties: 3,
+    losses: 50
+  }, {
+    TFClass: 'Spy',
+    wins: 28,
+    ties: 2,
+    losses: 29
+  }
+]
+
 interface ProfileProps {
   steamid: string;
 }
@@ -111,6 +164,28 @@ class Profile extends React.Component<ProfileProps, ProfileState> {
                 </div>
               </div>
             </div>
+            <div>
+              <BarChart
+                width={600}
+                height={400}
+                reverseStackOrder={true}
+                data={data}
+              >
+                <XAxis dataKey="TFClass" tickSize={0} tickFormatter={() => null} tick={renderClassAxisTick} />
+                <YAxis />
+                {/* 
+                // @ts-ignore */}
+                <Tooltip
+                  cursor={{fill: 'var(--background)'}}
+                  contentStyle={{background: 'var(--background-light)', borderRadius: 4, border: 'none', boxShadow: 'var(--paper-1)'}}
+                  formatter={(value, prop: string) => [value, prop.charAt(0).toUpperCase() + prop.slice(1)]}
+                  itemSorter={(payload: TooltipPayload) => -payload.name.charCodeAt(0)}
+                />
+                <Bar dataKey="wins" stackId="a" fill="#4caf50" />
+                <Bar dataKey="ties" stackId="a" fill="#ffeb3b" />
+                <Bar dataKey="losses" stackId="a" fill="#f44336" />
+              </BarChart>
+            </div>
           </div>
         </div>
       </main>
@@ -123,5 +198,13 @@ class Profile extends React.Component<ProfileProps, ProfileState> {
     );
   }
 }
+
+const renderClassAxisTick = ({ x, y, payload }: { x: number, y: number, payload: {value: DraftTFClass} }) => {
+  return (
+    <svg x={x - 16} y={y + 4} width={32} height={32}>
+      <ClassIcon name={payload.value} />
+    </svg>
+  );
+};
 
 export default Profile;
